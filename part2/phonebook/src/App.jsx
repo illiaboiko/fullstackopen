fdbebaaa1d7db4e056f6e54c7a0d096ba5e03c1f
illiaboiko <illiaboiko.095@gmail.com>
2024-12-24 22:15:35 +0200

    completed tast 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import areTheseObjectsEqual from "./utility";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import InputForm from "./components/InputForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
@@ -66,7 +67,7 @@ const App = () => {
             .toLocaleLowerCase()
             .includes(filterQuery.toLocaleLowerCase())
         );
-
+        console.log('render', persons)
   return (
     <div>
       <h2>Phonebook</h2>
